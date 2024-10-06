06bab3aca3486629556cc7bbb1370b4831f0942c
ADMIN-PC\Admin <hakinam2701@gmail.com>
2024-10-07 02:35:55 +0700

    fix gome

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,15 +5,15 @@ function Header(){
         <>
             <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
                 <Container>
-                    <Navbar.Brand href="/">Sport Live</Navbar.Brand>
+                    <Navbar.Brand href="/stream_client/#">Sport Live</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="/">Home</Nav.Link>
-                            <Nav.Link href="/">About Us</Nav.Link>
+                            <Nav.Link href="/stream_client/#">Home</Nav.Link>
+                            <Nav.Link href="/stream_client/#">About Us</Nav.Link>
                         </Nav>
                         <Nav>
-                            <Nav.Link href="/">Dark memes</Nav.Link>
+                            <Nav.Link href="/stream_client/#">Dark memes</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
