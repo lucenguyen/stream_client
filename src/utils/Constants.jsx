25f7642a51954153b6d03eaa1ee4cafbab792943
viet_anh_pham <phamvietanh1605@gmail.com>
2024-10-09 23:59:09 +0700

    update route

--- src/utils/Constants.jsx
+++ src/utils/Constants.jsx
@@ -1,4 +1,4 @@
-import {createBrowserRouter} from "react-router-dom";
+import {createBrowserRouter, Navigate} from "react-router-dom";
 import StreamChannel from "../components/StreamChannel";
 import Home from "../components/Home";
 
@@ -15,4 +15,8 @@ export const router = createBrowserRouter([
         path: "stream_client/:id",
         element: <StreamChannel></StreamChannel>
     },
+    {
+        path: "*", 
+        element: <Navigate to="/" />, 
+      },
 ]);
